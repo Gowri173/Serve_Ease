9810b322f3009fdbd7c38455c9a2552ed8135bb9
Gowri173 <121547683+Gowri173@users.noreply.github.com>
2026-04-18 15:49:59 +0530

    Update CORS origin to production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://team-delta-mauve.vercel.app/', credentials: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
