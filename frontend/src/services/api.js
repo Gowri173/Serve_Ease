ee7be4e4af36974ca09f825a14680afd7c71910f
Gowri173 <121547683+Gowri173@users.noreply.github.com>
2026-04-20 01:53:45 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://team-delta-uzst.onrender.com/api',
   withCredentials: true, // important for cookies
 });
 
